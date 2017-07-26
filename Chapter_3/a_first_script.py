0a553f4eb5d3f0ba9f33993fa1a6d41fbc85139a
Harold schreckengost <hmschreck@MacBook-Pro.local>
2017-07-26 09:32:59 -0500

    Added a_first_script.py

---
+++ Chapter_3/a_first_script.py
@@ -0,0 +1,7 @@
+#A First python Script
+
+import sys #loads sys library
+print(sys.platform) #prints system platform (OS)
+print(2**100) #Prints 2^100
+x = 'Spam!'
+print(x * 8) # Makes a wonderful Monty Python reference
